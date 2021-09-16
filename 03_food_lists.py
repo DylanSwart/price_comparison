59bfd91d9f77c346406bd52266c01081d9cb1404
DylanSwart <swart.dylan@awatapu.school.nz>
2021-09-16 12:44:39 +1200

    food_lists

--- 03_food_lists.py
+++ 03_food_lists.py
@@ -1,1 +1,12 @@
 # Component 2 Food lists
+
+food = [
+    "Sea Salt Crackers",
+    "Griffins Snax",
+    "Pizza Shapes",
+    "Arnotts Cheds",
+    "Rosemary Wheat",
+    "Original Rice Crackers"
+]
+
+print(food)
