1bbe740589952508303cddd8f969f1dbc5ffbb79
DylanSwart <swart.dylan@awatapu.school.nz>
2021-09-21 09:46:27 +1200

    food price listv2

--- 04_food_list_pricev2.py
+++ 04_food_list_pricev2.py
@@ -26,3 +26,47 @@ food_price_dict = {
     'original rice crackers': 1.65
 }
 
+kg_price = {
+    'sea salt crackers': 10.81,
+    'griffin snax': 10,
+    'pizza shapes': 17.37,
+    'arnotts cheds': 15.96,
+    'rosemary wheat': 11.76,
+    'original rice crackers': 16.5
+}
+
+# Initialise variables
+food_ok = ""
+food = ""
+
+print(valid_food)
+print()
+
+# Loop program three times
+for item in range(0, 3):
+
+    # Ask used for desired snack
+    desired_food = input("Food: ").lower()
+
+    for var_list in valid_food:
+
+        # If chosen snack is in valid snacks return full response
+        if desired_food in var_list:
+
+            # Get full name of snack and put it in title case
+            food = var_list[0].title()
+            food_ok = "yes"
+            break
+
+        # If chosen snack is not in valid snack set snack ok to no
+        else:
+            food_ok = "no"
+
+    # If the snack is not ok ask question again
+    if food_ok == "yes":
+        print("Snack choice: ", food)
+
+    else:
+        print("Sorry that was not a option")
+
+
