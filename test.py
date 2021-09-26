9e5dde591659c35f316d776b829a7844ae403a57
DylanSwart <swart.dylan@awatapu.school.nz>
2021-09-27 09:57:14 +1300

    food price listv3 (Testing)

--- test.py
+++ test.py
@@ -1,42 +1,7 @@
 
-import re
 import pandas
 
-
-def string_check(choice, options):
-
-    is_valid = ""
-    chosen = ""
-
-    for var_list in options:
-
-        # If the food is in the list return full response
-        if choice in var_list:
-
-            # Get full name of food and put it in title case
-            chosen = var_list[0].title()
-            is_valid = "yes"
-            break
-
-        # If chosen option is not valid set to no
-        else:
-            is_valid = "no"
-
-    # If food is not ok ask question again
-    if is_valid == "yes":
-        return chosen
-
-    else:
-        return "Invalid choice"
-
-
-# Regular expressions
-number_regex = "^[1-9]"
-
-# Variables
-food_ok = ""
-food = ""
-name = "Dylan"
+all_names = ['Dylan', 'Ryan']
 
 sea_salt_crackers = []
 griffins_snax = []
@@ -47,27 +12,9 @@ original_race_crackers = []
 
 food_list = [sea_salt_crackers, griffins_snax, pizza_shapes, arnotts_cheds, rosemary_wheat, original_race_crackers]
 
-valid_food = [
-    ["sea salt crackers"],
-    ["griffins snax"],
-    ["pizza shapes"],
-    ["arnotts cheds"],
-    ["rosemary wheat"],
-    ["original rice crackers"]
-]
-
-
-food_price_dict = {
-    'sea salt crackers': 2,
-    'griffins snax': 2.5,
-    'pizza shapes': 3.3,
-    'arnotts cheds': 3.99,
-    'rosemary wheat': 2,
-    'original rice crackers': 1.65
-}
-
+# Data frames dictionary
 food_data_dict = {
-    'Name': name,
+    'Name': all_names,
     'Sea Salt Crackers': sea_salt_crackers,
     'Griffins Snax': griffins_snax,
     'Pizza Shapes': pizza_shapes,
@@ -76,63 +23,43 @@ food_data_dict = {
     'Original Rice Crackers': original_race_crackers
 }
 
-payment = [
-    ["cash", "ca"],
-    ["credit", "cr"]
+# cost of each snack
+food_price_dict = {
+    'Sea Salt Crackers': 2,
+    'Griffins Snax': 2.5,
+    'Pizza Shapes': 3.3,
+    'Arnotts Cheds': 3.99,
+    'Rosemary Wheat': 2,
+    'Original Rice Crackers': 1.65,
+}
+
+test_data = [
+    [[2, 'Pizza Shapes'], [1, 'Rosemary Wheat'], [1, 'Griffins Snax'], [2, 'Original Rice Crackers']],
+    [[3, 'Pizza Shapes'], [1, 'Sea Salt Crackers'], [1, 'Arnotts Cheds']]
 ]
 
-food_order = []
+count = 0
+for client_order in test_data:
 
-print(valid_food)
+    # Assume No snacks have been bought
+    for item in food_list:
+        item.append(0)
+
+    # Print snack list
+
+    # Get order hard coded for testing
+    food_order = test_data[count]
+    count += 1
+
+    for item in food_order:
+        if len(item) > 0:
+            to_find = (item[1])
+            amount = (item[0])
+            add_list = food_data_dict[to_find]
+            add_list[-1] = amount
+
 print()
-
-# Loop program three times
-for item in range(0, 3):
-
-    # If user input is yes ask what food they want
-
-    desired_food = ""
-    while desired_food != "quit":
-        # Ask user for desired food
-        desired_food = input("food: ").lower()
-
-        food_row = []
-
-        # Exit code
-        if desired_food == "quit":
-            break
-
-        if re.match(number_regex, desired_food):
-            amount = int(desired_food[0])
-            desired_food = desired_food[1:]
-
-        else:
-            amount = 1
-            desired_food = desired_food
-
-        # Check if food is valid
-        food_choice = string_check(desired_food, valid_food)
-
-        # Check if food number is valid
-        if amount >= 5:
-            print("Sorry we have a max of 4 of each food")
-            food_choice = "Invalid choice"
-
-        # Add food to list
-        amount_food = "{} {}".format(amount, food_choice)
-
-        # Add food and amount to list
-
-        food_row.append(amount)
-        food_row.append(food_choice)
-
-        # Check if food is not exit code
-        if food_choice != "quit" and food_choice != "Invalid choice":
-            food_order.append(food_row)
-
-# Show food order
 # Print details
-
 food_frame = pandas.DataFrame(food_data_dict)
 food_frame = food_frame.set_index('Name')
 
