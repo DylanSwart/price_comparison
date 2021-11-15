40f92295ad8cbea2710d950d6f81fd3ff7f68d00
DylanSwart <swart.dylan@awatapu.school.nz>
2021-11-16 09:55:47 +1300

    food_listsv2 (comment changes)

--- 03_food_listsv2.py
+++ 03_food_listsv2.py
@@ -20,26 +20,26 @@ print()
 # Loop program three times
 for item in range(0, 3):
 
-    # Ask used for desired snack
+    # Ask used for desired food
     desired_food = input("Food: ").lower()
 
     for var_list in valid_food:
 
-        # If chosen snack is in valid snacks return full response
+        # If chosen food is in valid food return full response
         if desired_food in var_list:
 
-            # Get full name of snack and put it in title case
+            # Get full name of food and put it in title case
             food = var_list[0].title()
             food_ok = "yes"
             break
 
-        # If chosen snack is not in valid snack set snack ok to no
+        # If chosen food is not in valid food set food ok to no
         else:
             food_ok = "no"
 
-    # If the snack is not ok ask question again
+    # If the food is not ok ask question again
     if food_ok == "yes":
-        print("Snack choice: ", food)
+        print("food choice: ", food)
 
     else:
         print("Sorry that was not a option")
